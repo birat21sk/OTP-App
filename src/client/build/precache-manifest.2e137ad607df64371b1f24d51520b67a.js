d2b15b915bcc4fd6ad224f20e3c590bbe1a255c3
birat21sk <birat.sk21@gmail.com>
2022-04-13 14:50:53 +0545

    Initial commit

---
+++ src/client/build/precache-manifest.2e137ad607df64371b1f24d51520b67a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83072ed3286d798f0320663cfba96944",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95d8d8dba263820f6c3c",
+    "url": "/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "2cbc27abcca2bac09198",
+    "url": "/static/css/main.17a832b5.chunk.css"
+  },
+  {
+    "revision": "95d8d8dba263820f6c3c",
+    "url": "/static/js/2.a84b3112.chunk.js"
+  },
+  {
+    "revision": "59fc29711a22a223e9f602decc11b79f",
+    "url": "/static/js/2.a84b3112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cbc27abcca2bac09198",
+    "url": "/static/js/main.d990467c.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
