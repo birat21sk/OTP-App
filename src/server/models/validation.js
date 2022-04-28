6f541ec28f093126dafc5a551f0988317055582b
birat21sk <birat.sk21@gmail.com>
2022-04-28 10:03:11 +0545

    status code update

--- src/server/models/validation.js
+++ src/server/models/validation.js
@@ -3,16 +3,16 @@ function validateData(data){
     const codeInt = parseInt(code); 
     
     if(code.length !== codeLength){
-        return {status:"400", error:{message:"The code is not 6 digits"}} 
+        return {status:400, error:{message:"The code is not 6 digits"}} 
     }
     else if(isNaN(code)){
-        return {status:"400", error:{message:"Not a number"}}  
+        return {status:400, error:{message:"Not a number"}}  
     } 
     else if(codeInt % 10 === 7){
-        return {status:"400", error:{message:"The code ends with digit '7'"}}  
+        return {status:400, error:{message:"The code ends with digit '7'"}}  
     } 
     else{
-        return {status:"200", error:{message:""}}  
+        return {status:200, error:{message:""}}  
     }
 }
 
