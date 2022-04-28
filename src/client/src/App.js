b8023680c6c7c41a80846ad643f0a1b95d85ddc2
birat21sk <birat.sk21@gmail.com>
2022-04-28 15:45:49 +0545

    Implementing Store

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react"; 
 import {Route, Switch} from 'react-router-dom'; 
-import Home from "./components/home";
-import Success from "./components/success";
+import Home from "./components/Home";
+import Success from "./components/Success";
+
 import "./App.css";
 
-const App=()=> {
+const App=()=> { 
   return ( 
     <Switch>
       <Route path="/success" component={Success}/>
--- src/client/src/index.js
+++ src/client/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
+import StoreProvider from "./store/configStore";
+
 import './index.css'; 
 import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <StoreProvider>
+            <App />
+        </StoreProvider>
     </BrowserRouter>, 
     document.getElementById('root')
 );
